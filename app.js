d69b918fc9f1ddc8d1e53e3020668926c98c2b04
Alex Widener <awidener3@gmail.com>
2021-11-08 12:15:00 -0500

    fixed remove button functionality

--- app.js
+++ app.js
@@ -1,154 +1,142 @@
-// variable declarations
-const bookContainer = document.querySelector('.book-container');
-const addBook_button = document.querySelector('.add-book');
-const submitBook_button = document.getElementById('submit');
-const inputs_form = document.querySelectorAll('input');
-const overlay_div = document.getElementById('overlay');
+const bookContainer = document.getElementById('book-container');
+const addBookButton = document.getElementById('add-book-btn');
+const submitButton = document.getElementById('submit');
+// const removeButton = document.getElementById('remove');
+const readButton  = document.getElementById('read-btn');
+const modal = document.getElementById('overlay');
+const closeModal = document.getElementsByClassName("close")[0];
 
-// book storage
-let myLibrary = [
-    {
-        'bookTitle': 'The Hobbit',
-        'author': 'J.R.R. Tolkien',
-        'pages': 297,
-        'readStatus': 'read'
-    },
-    {
-        'bookTitle': 'Dune',
-        'author': 'Frank Hubert',
-        'pages': 412,
-        'readStatus': 'not read'
-    },
-    {
-        'bookTitle': 'Mistborn',
-        'author': 'Brandon Sanderson',
-        'pages': 1161,
-        'readStatus': 'not read'
-    }
-];
+// modal functions
 
-/* overlay and form */
-
-function turnOnOverlay() {
-    document.getElementById('overlay').style.display = 'block';
+addBookButton.onclick = function() {
+    document.getElementById('input-form').reset();  // reset form
+    modal.style.display = 'block';                  // reveal modal
 }
-
-function turnOffOverlay() {
-    document.getElementById('overlay').style.display = 'none';
+closeModal.onclick = function() {
+    modal.style.display = 'none';
 }
-
-// closes overlay if clicked anywhere but form
-window.onclick = function(event) {
-    if (event.target == overlay_div) {
-        overlay_div.style.display = 'none';
+window.onclick = function(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
     }
 }
+submitButton.onclick = function() {
+    addBookToLibrary();
+    modal.style.display = 'none';
+}
 
-/* book constructor */
-function Book(bookTitle, author, pages, readStatus) { // <-- just added parameters
-    this.bookTitle = bookTitle,
+let myLibrary = [];
+
+
+// Book Constructor & Prototypes //
+
+function Book(title, author, pages, isRead) {
+    this.title = title,
     this.author = author,
     this.pages = pages,
-    this.readStatus = readStatus
-};
+    this.isRead = isRead
+}
 
-/* book object creation */
+Book.prototype.toggleRead = function() {
+    console.log('toggleRead activate!');
+    console.log(this.isRead);
+
+    switch(this.isRead) {
+        case 'true':
+        case true:
+            this.isRead = false;
+            break;
+        case 'false':
+        case false:
+            this.isRead = true;
+            break;
+    }
+
+    console.log('toggleRead processed!');
+    console.log(this.isRead);
+    // this.isRead ? false : true;
+}
 
 function addBookToLibrary() {
-    if (document.getElementById('book-title').value == '') {
-        return;
-    }
-    this.bookTitle = document.getElementById('book-title').value;
-    this.author = document.getElementById('book-author').value;
-    this.pages = document.getElementById('book-pages').value;
-    this.readStatus = document.getElementById('book-status').value;
-    let newBook = new Book();
+    let title = document.getElementById('book-title').value;
+    let author = document.getElementById('book-author').value;
+    let pages = document.getElementById('book-pages').value;
+    let isRead = document.getElementById('book-status').value;
+    
+    const newBook = new Book(title, author, pages, isRead)
+    createCard(title, author, pages, isRead);
+    
     myLibrary.push(newBook);
-    turnOffOverlay();
-    updateDisplay();
-};
-
-/* myLibrary Display */
-
-// function updateDisplay() {
-//     bookContainer.innerHTML = '';
-//     for (let i = 0; i < myLibrary.length; i++) {
-//         const card = document.createElement('div');
-//         card.classList = 'card';
-//         card.innerHTML += 
-//             `<img class="card-book" src="images/blank-book.png" alt="A blank book">
-//             <span class="card-hover">
-//                 <div class="card-title">${myLibrary[i].bookTitle}</div>
-//                 <div class="card-author">${myLibrary[i].author}</div>
-//                 <div class="card-pages">${myLibrary[i].pages} pages</div>
-//                 <button class="card-status-button" data-read-status="${myLibrary[i].readStatus}">${myLibrary[i].readStatus}</button>
-//             </span>`
-//         if (myLibrary[i].readStatus == 'read') {
-//             card.innerHTML += `<div id="checked" class="check" onclick="toggleCheck(this)">✓</div>`;
-//         } else {
-//             card.innerHTML += `<div id="unchecked" class="blank-check" onclick="toggleCheck(this)"></div>`
-//         }
-//         bookContainer.appendChild(card);
-//     }
-// }
-
-function updateDisplay() {
-    bookContainer.innerHTML = '';
-    for (let i = 0; i < myLibrary.length; i++) {
-        const card = document.createElement('div');
-        card.classList = 'card';
-        card.innerHTML += 
-            `<img class="card-book" src="images/blank-book.png" alt="A blank book">
-            <span class="card-hover">
-                <div class="card-title">${myLibrary[i].bookTitle}</div>
-                <div class="card-author">${myLibrary[i].author}</div>
-                <div class="card-pages">${myLibrary[i].pages} pages</div>
-                <div id="checkbox" class="card-status-button" data-readstatus="${myLibrary[i].readStatus}" onclick="toggleCheck(this)"></div>
-                <button class="card-status-button" data-read-status="${myLibrary[i].readStatus}">${myLibrary[i].readStatus}</button>
-            </span>`
-        bookContainer.appendChild(card);
-    }
+    console.log(`current library:`);
+    console.log(myLibrary);
 }
 
-updateDisplay();
+function deleteBook(card) {
+    card.parentNode.parentNode.removeChild(card.parentNode);
+    for (let book of myLibrary) {
+        if (book.title == card.parentNode.dataset.title && book.author == card.parentNode.dataset.author) {
+            let index = myLibrary.indexOf(book);
+            myLibrary.splice(index, 1)
+        }
+    }
+    console.log(`resulting library:`);
+    console.log(myLibrary);
+}
 
-/* Misc buttons */
+function createCard(title, author, pages, isRead) {
+    const bookCard = document.createElement('div');
+    const bookTitle = document.createElement('h2');
+    const bookAuthor = document.createElement('h3');
+    const bookPages = document.createElement('p');
+    const readButton = document.createElement('button');
+    const removeButton = document.createElement('button');
 
-function toggleCheck(elem) {
-    if (elem.classList.contains('check')) {
-        elem.classList.remove('check');
-        elem.classList.add('blank-check');
-        elem.textContent = '';
+    bookCard.classList.add('card');
+    bookCard.setAttribute('data-title', `${title}`);
+    bookCard.setAttribute('data-author', `${author}`);
+    bookTitle.classList.add('book-title');
+    bookAuthor.classList.add('book-author');
+    bookPages.classList.add('book-pages');
+    readButton.classList.add('read-btn');
+    removeButton.classList.add('remove-btn');
+    
+    removeButton.onclick = function() {
+        deleteBook(this);
+    }
+
+    readButton.onclick = function() {
+        // console.log(this.parentNode);
+        for (let book of myLibrary) {
+            book.toggleRead();
+            
+        }
+    }
+
+    if (isRead === 'true') {
+        isRead = 'Read';
+        readButton.classList.add('green-btn');
     } else {
-        elem.classList.remove('blank-check');
-        elem.classList.add('check');
-        elem.textContent = '✓';
+        isRead = 'Not Read';
+        readButton.classList.add('red-btn');
     }
+
+    bookTitle.textContent = `"${title}"`;
+    bookAuthor.textContent = author;
+    bookPages.textContent = `${pages} pages`;
+    readButton.textContent = isRead;
+    removeButton.textContent = 'Remove';
+
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(readButton);
+    bookCard.appendChild(removeButton);
+    bookContainer.appendChild(bookCard);
 }
 
-// sample books //
 
-// 'Outliers', 'Malcolm Gladwell', 309, 'not read'
-// 'Grit', 'Angela Duckworth', 277, 'in-progress'
-// 'The Hobbit', 'J.R.R. Tolkien', 297, 'read'
-// 'Dune', 'Frank Hubert', 412, 'in-progress'
-// 'Mistborn', 'Brandon Sanderson', 1161, 'not read'
+// // // // // 
 
-// {
-//     'bookTitle': 'The Hobbit',
-//     'author': 'J.R.R. Tolkien',
-//     'pages': 297,
-//     'readStatus': 'read'
-// },
-// {
-//     'bookTitle': 'Dune',
-//     'author': 'Frank Hubert',
-//     'pages': 412,
-//     'readStatus': 'in-progress'
-// },
-// {
-//     'bookTitle': 'Mistborn',
-//     'author': 'Brandon Sanderson',
-//     'pages': 1161,
-//     'readStatus': 'not read'
-// }
+// addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 512, true);
+// addBookToLibrary('Grit', 'Angela Duckworth', 422, false);
+// addBookToLibrary('Outliers', 'Malcom Gladwell', 143, false);
